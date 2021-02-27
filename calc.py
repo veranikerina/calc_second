8fc56801a56e3d0c4633e489e715e8d863b0b488
Vera Nikerina <nikerina.vera@gmail.com>
2021-02-27 11:06:17 +0300

    First commit of DIV

---
+++ calc.py
@@ -0,0 +1,14 @@
+str_input = input("Delimoe: ")
+
+delimoe = int(str_input)
+#print(type(delimoe))
+
+str_input2 = input("Delitel: ")
+Delitel: 234
+delitel = int(str_input2)
+#print(type(delitel))
+
+result = delimoe / delitel
+#print(type(result))
+
+print("Result: "+str(result))
