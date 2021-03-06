5169489a6e6c85ac858af67afd7761bfedd73692
Vera Nikerina <nikerina.vera@gmail.com>
2021-03-06 12:26:49 +0300

    new commit 12:30

--- calc.py
+++ calc.py
@@ -1,15 +1,36 @@
 #simple div
 
-str_input = input("A: ")
+str_command = input ("Please type command a + b or a - b: ")
 
-delimoe = float(str_input)
+str_A = ''
+str_B = ''
+operation = ''
+
+#2 + 3
+
+for letter in str_command:
+    print(letter)
+    if letter == '+' or letter == '-' or letter == '*' or letter == '/' or letter == '^':
+        operation = letter
+    else:
+        if operation == '':
+            str_A = str_A + letter
+        else:
+            str_B = str_B + letter
+
+str_A.strip()
+str_B.strip()
+print(str_A)
+print(str_B)
+
+#str_input = input("A: ")
+
+delimoe = float(str_A)
 #print(type(delimoe))
+#operation = input("+ / * - ^: ")
+#str_input2 = input("B: ")
 
-operation = input("+ / * - ^: ")
-
-str_input2 = input("B: ")
-
-delitel = float(str_input2)
+delitel = float(str_B)
 #print(type(delitel))
 result = None
 
