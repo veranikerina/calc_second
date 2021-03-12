8011c786bb39c5a8f92e662fa5902efdbae36880
Vera Nikerina <nikerina.vera@gmail.com>
2021-03-12 21:35:53 +0300

    new commit 21:37

--- calc.py
+++ calc.py
@@ -1,6 +1,4 @@
-#simple div
-
-str_command = input ("Please type command a + b or a - b: ")
+str_command = input ("Please type command a + b or a - b: ").replace(' ','')
 
 type_A = ''
 str_A = ''
@@ -9,38 +7,26 @@ type_B = ''
 str_B = ''
 
 operation = ''
+i = 0
 
-#2 + 3
-
-for letter in str_command:
-    print(letter)
-    if letter == '+' or letter == '-' or letter == '*' or letter == '/' or letter == '^':
+while i < len(str_command) :
+    if str_command[i] == '+' or str_command[i] == '-' or str_command[i] == '*' or str_command[i] == '/' or str_command[i] == '^':
         if str_A == '':
-            type_A = letter
+            type_A = str_command[i]
         elif operation != '':
-            type_B = letter
+            type_B = str_command[i]
         else:
-            operation = letter
+            operation = str_command[i]
     else:
         if operation == '':
-            str_A = str_A + letter
+            str_A += str_command[i]
         else:
-            str_B = str_B + letter
+            str_B += str_command[i]
+    i += 1
 
-str_A = type_A + str_A.strip()
-str_B = type_B + str_B.strip()
-print(str_A)
-print(str_B)
+delimoe = float(type_A + str_A)
+delitel = float(type_B + str_B)
 
-#str_input = input("A: ")
-
-delimoe = float(str_A)
-#print(type(delimoe))
-#operation = input("+ / * - ^: ")
-#str_input2 = input("B: ")
-
-delitel = float(str_B)
-#print(type(delitel))
 result = None
 
 if operation == '/':
@@ -58,5 +44,5 @@ elif operation == '^':
     result = delimoe ** delitel
 else:
     result = "unknown"
-#print(type(result))
+
 print("Result: " + str(result))
